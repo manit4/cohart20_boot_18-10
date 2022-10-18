0748ad40e1e8f51f6ecfcdb10a290b7bf736055c
Manit <manit.manu4@gmail.com>
2022-10-18 19:46:04 +0530

    first_18-10

---
+++ src/main/java/com/cohart20/First.java
@@ -0,0 +1,49 @@
+package com.cohart20;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cohart20.pojo.User;
+
+@RestController
+public class First {
+	
+	@RequestMapping("/message")
+	public String getMessage() {
+		return "Hello SpringBoot Developers :)";
+	}
+	
+	@RequestMapping("/getTodayWeather")
+	public String getWeather() {
+		
+		return "Today, the weather will be really hot and keep your sun-screen along...";
+	}
+	
+	@RequestMapping("/getTemperature")
+	public Double getTemp() {
+		return 35.56;
+	}
+	
+	@RequestMapping("/getUser")
+	public User gertUserDetails() {
+		return new User(100, "ama@123", "Ama Gunasekhara", "ama@yahoo.com");
+				
+	}
+	
+	@RequestMapping("/getUsers")
+	public List<User> getUsers() {
+		
+		User user1 = new User(1, "dan@123", "Danielle", "dani@gmail.com");
+		User user2 = new User(2, "fal@234", "Falguni", "fal@gmail.com");
+		User user3 = new User(3, "adam@345", "Adam", "ada@yahoo.com");
+		
+		List<User> users = new ArrayList<User>();
+		users.add(user1);   users.add(user2);  users.add(user3);
+		
+		return users;	
+	}
+
+}
